84cf54bbb5164cfa533788ef9ac14c599ca07d37
superpuper32 <pochitalin_m@mail.ru>
2018-04-27 23:08:56 +0300

    getNeighborPads() function completed

--- public/js/script.js
+++ public/js/script.js
@@ -47,5 +47,19 @@ for (let i = 0; i < drums.length; i++) {
 };
 
 const getNeighborPads = (x, y, size) => {
-  
+  const neighborPads = [];
+  if (x >= size || y >= size || x < 0 || y < 0 || size < 1) {
+    return neighborPads;
+  }
+
+  neighborPads.push([x - 1, y]);
+  neighborPads.push([x, y - 1]);
+  neighborPads.push([x + 1, y]);
+  neighborPads.push([x, y + 1]);
+
+  return neighborPads.filter((neighbor) => {
+    return neighbor.every((val) => {
+      return val >= 0 && val < size;
+    });
+  });
 };
